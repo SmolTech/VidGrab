1d9b884f8ee9c8159bd18d644db26b10308aa09f
Gregory Boyce <gregory@smoltech.us>
2026-07-12 10:20:04 -0400

    chore: bump version to 1.1.2

--- VidGrab/app/build.gradle.kts
+++ VidGrab/app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "us.smoltech.vidgrab"
         minSdk = 26
         targetSdk = 36
-        versionCode = 3
-        versionName = "1.1.1"
+        versionCode = 4
+        versionName = "1.1.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
