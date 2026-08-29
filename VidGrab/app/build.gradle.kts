8fed6e732f3d8da1b6a11b2b71eea21fc1af4165
Gregory Boyce <gregory@smoltech.us>
2026-08-29 19:51:43 -0400

    Update version to 1.1.6

--- VidGrab/app/build.gradle.kts
+++ VidGrab/app/build.gradle.kts
@@ -15,7 +15,7 @@ android {
         minSdk = 26
         targetSdk = 36
         versionCode = 5
-        versionName = "1.1.3"
+        versionName = "1.1.6"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
